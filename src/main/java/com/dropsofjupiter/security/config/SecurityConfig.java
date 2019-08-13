47a3fde8cf90431da7089da123fa8eb629288e64
邱恬恬 <qiutt@kaopuyun.com>
2019-08-13 10:01:55 +0800

    ‘第一次提交文件到本地库中去’

---
+++ src/main/java/com/dropsofjupiter/security/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.dropsofjupiter.security.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author qiutt
+ * @description:no description
+ * @create 2019-08-12 16:48
+ */
+//@Configuration
+//@EnableWebSecurity
+//@Slf4j
+public class SecurityConfig{
+}
